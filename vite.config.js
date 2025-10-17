a8b04cee223dc7c2138f4217c6bf9d9d6451100d
Mahmoud Sabry <mahmoudelkhwass704@gmail.com>
2025-10-17 20:58:12 +0300

    Configure for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: './', // For GitHub Pages compatibility
+  base: '/first-project-vue/', // For GitHub Pages compatibility
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
